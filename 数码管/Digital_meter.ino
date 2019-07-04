3e9a067711c0a2567e90b4f5c2a3dee5e5fb5fc0
Xudong0722 <49638560+Xudong0722@users.noreply.github.com>
2019-07-04 22:29:14 +0800

    Update Digital_meter.ino

--- 数码管/Digital_meter.ino
+++ 数码管/Digital_meter.ino
@@ -1,4 +1,4 @@
-void setup()
+/*void setup()
 {
   pinMode(6, OUTPUT);
   pinMode(5, OUTPUT);
@@ -36,3 +36,102 @@ void loop()
     delay(1000);
   }
 }
+*/
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  Serial.begin(9600);
+}
+void loop()
+  {
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  int income=0;
+  if(Serial.available()>0)
+  {
+    delay(10);
+    income=Serial.read();
+  }
+  switch(income){
+    case'0':
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+    delay(1000);
+    break;
+    case'1':
+    digitalWrite(2,HIGH);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+    delay(1000);
+    break;
+    case'2':
+    digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+    delay(1000);
+    break;
+    case'3':
+    digitalWrite(2,HIGH);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+    delay(1000);
+    break;
+    case'4':
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(5,LOW);
+    delay(1000);
+    break;
+    case'5':
+    digitalWrite(2,HIGH);
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(5,LOW);
+    delay(1000);
+    break;
+    case'6':
+    digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,HIGH);
+    digitalWrite(5,LOW);
+    delay(1000);
+    break;
+    case'7':
+    digitalWrite(2,HIGH);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,HIGH);
+    digitalWrite(5,LOW);
+    delay(1000);
+    break;
+    case'8':
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,HIGH);
+    delay(1000);
+    break;
+    case'9':
+    digitalWrite(2,HIGH);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,HIGH);
+    delay(1000);
+
+    break;
+
+  }
+
+ }
